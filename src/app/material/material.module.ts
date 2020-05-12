361aef47d837136c19f3fa5c3c4312ace66faba0
=JusticeSelormBruce <=justiceselormbruce@gmail.com>
2020-05-12 01:29:54 +0000

    Retouch on general design layout and Adding Skills section

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import { MatStepperModule } from "@angular/material/stepper";
 import  {MatFormFieldModule} from '@angular/material/form-field';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 
 @NgModule({
@@ -13,13 +15,15 @@ import  {MatFormFieldModule} from '@angular/material/form-field';
     MatToolbarModule,
     MatCardModule,
     MatStepperModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatExpansionModule
   ],
   exports:[
     MatToolbarModule,
     MatCardModule,
     MatStepperModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule { }
--- src/app/models/technologies/technologies.component.ts
+++ src/app/models/technologies/technologies.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-technologies',
   templateUrl: './technologies.component.html',
-  styleUrls: ['./technologies.component.css']
+  styleUrls: ['./technologies.component.css'],
 })
 export class TechnologiesComponent implements OnInit {
+  bg = 'assets/languages.png';
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
-
 }
